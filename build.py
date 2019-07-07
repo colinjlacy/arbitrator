f00863bf4537b91883d13b0ef4388efa5402441e
Colin Lacy <colinjlacy@gmail.com>
2019-07-07 14:35:11 -0500

    sets up start and stop scripts, simplifies test for debugging

--- build.py
+++ build.py
@@ -6,19 +6,8 @@ if __name__ == '__main__':
     nlp = NaturalLanguageProcessor("./code_domain_emissary")
     nlp.build()
 
-    res4 = nlp.process("bye")
-    # res5 = nlp.process("Good bye!")
-    # res6 = nlp.process("quit")
+    res1 = nlp.process("hello")
+    assert res1.get("intent") == "create_element"
 
-    assert res4.get("intent") == "delete_element"
-    # assert res5.get("intent") == "delete_element"
-    # assert res6.get("intent") == "delete_element"
-
-    # res1 = nlp.process("hello")
-    # res2 = nlp.process("hello?")
-    # res3 = nlp.process("Hello!")
-    #
-    # assert res1.get("intent") == "edit_element"
-    # assert res2.get("intent") == "edit_element"
-    # assert res3.get("intent") == "edit_element"
-
+    res2 = nlp.process("bye")
+    assert res2.get("intent") == "delete_element"
---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+mindmeld num-parse
+./elasticsearch-6.7.0/bin/elasticsearch
