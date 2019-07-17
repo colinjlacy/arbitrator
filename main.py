b703fd9be808fb599818464e186adbd7c1ad5b24
Colin Lacy <colinjlacy@gmail.com>
2019-07-16 20:35:17 -0500

    experiments with tokens

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from code_domain_emissary.emissary import Emissary
 
 if __name__ == '__main__':
     em = Emissary()
-    doc = em.process("My name is Colin")
-    print(doc)
+    doc = em.process("create a function named get user account with the arguments user ID and include savings")
+    print(doc.ents)
+    print(list(doc.noun_chunks))
+    for t in doc:
+        print(t, [child for child in t.children], t.dep_, t.pos_)
 
